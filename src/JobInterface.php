c4461e26949502800394a39f2e55d8cc77521401
Luke Richards <luke.richards@emailcenteruk.com>
2015-12-14 16:05:59 +0000

    Expend job interface
    Added additional methods to the interface to improve it as a data
    transfer object.

--- src/JobInterface.php
+++ src/JobInterface.php
@@ -5,12 +5,50 @@ namespace Phlib\JobQueue;
 interface JobInterface
 {
     /**
-     * @return string
+     * @return string|integer|null
      */
     public function getId();
 
     /**
      * @return string
      */
+    public function getQueue();
+
+    /**
+     * @return string
+     */
     public function getBody();
+
+    /**
+     * @return \DateTime
+     */
+    public function getDelay();
+
+    /**
+     * @param \DateTime $value
+     * @return $this
+     */
+    public function setDelay(\DateTime $value);
+
+    /**
+     * @return int
+     */
+    public function getTtr();
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setTtr($value);
+
+    /**
+     * @return int
+     */
+    public function getPriority();
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setPriority($value);
 }
