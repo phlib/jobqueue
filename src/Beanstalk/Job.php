33e2930dfb9da3c6ed869f57c72cfb0b346cb61f
Luke Richards <luke.richards@emailcenteruk.com>
2016-01-04 17:10:35 +0000

    Update to implement interface changes.
    Change implementation of beanstalk job to include new interface methods
    and related changes.

--- src/Beanstalk/Job.php
+++ src/Beanstalk/Job.php
@@ -2,6 +2,7 @@
 
 namespace Phlib\JobQueue\Beanstalk;
 
+use Phlib\Beanstalk\Connection;
 use Phlib\JobQueue\JobInterface;
 
 /**
@@ -11,23 +12,82 @@ use Phlib\JobQueue\JobInterface;
 class Job implements JobInterface
 {
     /**
-     * @var array
+     * @var string|null
      */
-    private $id;
+    protected $queue = null;
 
     /**
-     * @var array
+     * @var int|string|null
      */
-    private $body;
+    protected $id;
 
-    public function __construct(array $data)
+    /**
+     * @var int
+     */
+    protected $delay;
+
+    /**
+     * @var int
+     */
+    protected $priority;
+
+    /**
+     * @var int
+     */
+    protected $ttr;
+
+    /**
+     * @param array $data
+     * @return self
+     */
+    public static function createFromSpecification(array $data)
     {
-        $this->id = $data['id'];
-        $this->body = $data['body'];
+        if (!isset($data['body']) || !isset($data['queue'])) {
+            throw new \InvalidArgumentException('Missing required job data.');
+        }
+
+        $id = null;
+        if (isset($data['id'])) {
+            $id = $data['id'];
+        }
+
+        // merge default values if any are missing
+        $data = $data + [
+            'delay'    => Connection::DEFAULT_DELAY,
+            'priority' => Connection::DEFAULT_PRIORITY,
+            'ttr'      => Connection::DEFAULT_TTR
+        ];
+
+        return new static($data['queue'], $data['body'], $id, $data['delay'], $data['priority'], $data['ttr']);
     }
 
     /**
-     * @return string
+     * Job constructor.
+     * @param string $queue
+     * @param mixed $body
+     * @param int|string|null $id
+     * @param int $delay
+     * @param int $priority
+     * @param int $ttr
+     */
+    public function __construct(
+        $queue,
+        $body,
+        $id = null,
+        $delay = Connection::DEFAULT_DELAY,
+        $priority = Connection::DEFAULT_PRIORITY,
+        $ttr = Connection::DEFAULT_TTR
+    ) {
+        $this->queue = $queue;
+        $this->body  = $body;
+        $this->id    = $id;
+        $this->delay = (int)$delay;
+        $this->setPriority($priority);
+        $this->setTtr($ttr);
+    }
+
+    /**
+     * @return int|string|null
      */
     public function getId()
     {
@@ -37,8 +97,106 @@ class Job implements JobInterface
     /**
      * @return string
      */
+    public function getQueue()
+    {
+        return $this->queue;
+    }
+
+    /**
+     * @return string
+     */
     public function getBody()
     {
         return $this->body;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDelay()
+    {
+        return $this->delay;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getDatetimeDelay()
+    {
+        return (new \DateTimeImmutable())->setTimestamp(time() + $this->getDelay());
+    }
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setDelay($value)
+    {
+        $this->delay = (int)$value;
+        return $this;
+    }
+
+    /**
+     * @param \DateTimeInterface $value
+     * @return $this
+     */
+    public function setDatetimeDelay(\DateTimeInterface $value)
+    {
+        $value = time() - $value->getTimestamp();
+        if ($value < 0) {
+            $value = 0;
+        }
+        $this->setDelay($value);
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTtr()
+    {
+        return $this->ttr;
+    }
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setTtr($value)
+    {
+        $this->ttr = (int)$value;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setPriority($value)
+    {
+        $this->priority = (int)$value;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function toSpecification()
+    {
+        return [
+            'queue'    => $this->queue,
+            'body'     => $this->body,
+            'delay'    => $this->delay,
+            'priority' => $this->priority,
+            'ttr'      => $this->ttr
+        ];
     }
 }
