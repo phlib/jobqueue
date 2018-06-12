321672db2a6792ef079b96c379eff1fe40a5a1a3
Luke <luke.richards@qubit.co.uk>
2018-06-12 12:47:32 +0100

    Change to make logging an optional input
    Added a option to the command to specify the location of the log file.
    If it’s not specified then the default NullLogger is used from the
    parent DaemonCommand.

--- src/Console/MonitorCommand.php
+++ src/Console/MonitorCommand.php
@@ -7,6 +7,7 @@ use Phlib\JobQueue\JobQueueInterface;
 use Phlib\JobQueue\Exception\InvalidArgumentException;
 use Phlib\ConsoleProcess\Command\DaemonCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\StreamOutput;
 
@@ -22,6 +23,11 @@ class MonitorCommand extends DaemonCommand
      */
     protected $jobQueue;
 
+    /**
+     * @var string
+     */
+    protected $logFile;
+
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
         $dependencies = $this->getHelper('configuration')
@@ -31,14 +37,16 @@ class MonitorCommand extends DaemonCommand
             throw new InvalidArgumentException('Expected dependencies could not be determined.');
         }
 
-        $this->jobQueue = $dependencies->getJobQueue();
+        $this->jobQueue  = $dependencies->getJobQueue();
         $this->scheduler = $dependencies->getScheduler();
     }
 
     protected function configure()
     {
         $this->setName('monitor')
-            ->setDescription('Monitor the schedule for pending jobs.');
+            ->setDescription('Monitor the schedule for pending jobs.')
+            ->addOption('log', 'l', InputOption::VALUE_REQUIRED, "A file to send log out to. If no path is specified then it's disabled.");
+        ;
     }
 
     /**
@@ -46,6 +54,11 @@ class MonitorCommand extends DaemonCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $logFile = $input->getOption('log');
+        if (!empty($logFile)) {
+            $this->logFile = $logFile;
+        }
+
         while ($jobData = $this->scheduler->retrieve()) {
             $output->writeln("Job {$jobData['id']} added.");
             $this->jobQueue->put($this->createJob($jobData));
@@ -66,10 +79,13 @@ class MonitorCommand extends DaemonCommand
     }
 
     /**
-     * @return ConsoleOutputInterface
+     * @return OutputInterface
      */
     protected function createChildOutput()
     {
-        return new StreamOutput(fopen(getcwd() . '/jobqueue-monitor.log', 'a'));
+        if (empty($this->logFile)) {
+            return parent::createChildOutput();
+        }
+        return new StreamOutput(fopen($this->logFile, 'ab'));
     }
 }
