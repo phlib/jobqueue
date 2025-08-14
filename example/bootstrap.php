1ca7bce1f2c311a4da0a681e028686eb0f439bba
Xnopyt <billy@xnopyt.info>
2025-08-14 17:49:55 +0100

    Add retry on deadlock support to DbScheduler

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -5,5 +5,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $queue     = 'my-test-tube';
 $beanstalk = (new \Phlib\Beanstalk\Factory())->create('localhost');
 $db        = new \Phlib\Db\Adapter(['host' => '127.0.0.1', 'dbname' => 'test']);
-$scheduler = new \Phlib\JobQueue\Scheduler\DbScheduler($db, 60, 120);
+$backoff   = new \STS\Backoff\Backoff(5, 'constant', 2000, true);
+$scheduler = new \Phlib\JobQueue\Scheduler\DbScheduler($db, 60, 120, true, 50, $backoff);
 $jobQueue  = new \Phlib\JobQueue\Beanstalk\JobQueue($beanstalk, $scheduler);
